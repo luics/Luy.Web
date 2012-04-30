6d9d6aea88705f8c79c9534b16de1c8b9ed2a1aa
luics <luics.king@gmail.com>
2012-04-30 17:18:14 +0800

    backup

--- books/NodeJsBeginner/requestHandlers.js
+++ books/NodeJsBeginner/requestHandlers.js
@@ -2,28 +2,28 @@ var querystring = require("querystring"),
 fs = require("fs"),
 formidable = require("formidable"),
 url = require("url");
-    
+
 var UPLOAD = 'D:/Documents/NetBeansProjects/Luy.Web.git/books/NodeJsBeginner/tmp';
 
 function start(response) {
   console.log("Request handler 'start' was called.");
-
-  var body = '<html>'+
-  '<head>'+
-  '<meta http-equiv="Content-Type" '+
-  'content="text/html; charset=UTF-8" />'+
-  '</head>'+
-  '<body>'+
-  '<form action="/upload" enctype="multipart/form-data" '+
-  'method="post">'+
-  '<input type="file" name="upload" multiple="multiple">'+
-  '<input type="submit" value="Upload file" />'+
-  '</form>'+
-  '</body>'+
-  '</html>';
-
+  
+  var body = '<html>' +
+    '<head>' +
+    '<meta http-equiv="Content-Type" ' +
+    'content="text/html; charset=UTF-8" />' +
+    '</head>' +
+    '<body>' +
+    '<form action="/upload" enctype="multipart/form-data" ' +
+    'method="post">' +
+    '<input type="file" name="upload" multiple="multiple">' +
+    '<input type="submit" value="Upload file" />' +
+    '</form>' +
+    '</body>' +
+    '</html>';
+  
   response.writeHead(200, {
-    "Content-Type": "text/html"
+    "Content-Type" : "text/html"
   });
   response.write(body);
   response.end();
@@ -31,26 +31,27 @@ function start(response) {
 
 function upload(response, request) {
   console.log("Request handler 'upload' was called.");
-
+  
   var form = new formidable.IncomingForm();
   console.log("about to parse");
-  form.parse(request, function(error, fields, files) {
+  form.parse(request, function (error, fields, files) {
     console.log("parsing done");
-
-    var file = (+ new Date);
+    
+    var file = ( + new Date);
     /* Possible error on Windows systems:
-       tried to rename to an already existing file */
-    fs.rename(files.upload.path, UPLOAD + '/' + file, function(err) {
+    tried to rename to an already existing file */
+    fs.rename(files.upload.path, UPLOAD + '/' + file, function (err) {
       if (err) {
         fs.unlink(UPLOAD + '/' + file);
         fs.rename(files.upload.path, UPLOAD + '/' + file);
       }
     });
+	
     response.writeHead(200, {
-      "Content-Type": "text/html"
+      "Content-Type" : "text/html"
     });
     response.write("received image:<br/>");
-    response.write("<img src='/show' />");
+    response.write('<img src="/show?file=' + file + '" />');
     response.end();
   });
 }
@@ -63,29 +64,29 @@ function show(response, request) {
   responseFile(response, filename);
 }
 
-function responseFile(response, filename){
+function responseFile(response, filename) {
   console.log("responseFile");
   
-  fs.readFile(UPLOAD + '/' + filename, "binary", function(error, file) {
-    if(error) {
+  fs.readFile(UPLOAD + '/' + filename, "binary", function (error, file) {
+    if (error) {
       response.writeHead(500, {
-        "Content-Type": "text/plain"
+        "Content-Type" : "text/plain"
       });
       response.write(error + "\n");
       response.end();
     } else {
       response.writeHead(200, {
-        "Content-Type": "image/png"
+        "Content-Type" : "image/png"
       });
       response.write(file, "binary");
       response.end();
     }
-  });  
+  });
 }
 
 function favicon(response, request) {
   console.log("Request handler 'favicon' was called.");
-  responseFile(response, 'favicon.ico');  
+  responseFile(response, 'favicon.ico');
 }
 
 exports.start = start;
