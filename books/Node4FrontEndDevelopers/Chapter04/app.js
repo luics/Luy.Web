ae3692ccc19f2a0f65b4160dd31c4e6206c8e5f5
luics <luics.king@gmail.com>
2012-05-04 17:45:17 +0000

    node 2 fe, chapter04

---
+++ books/Node4FrontEndDevelopers/Chapter04/app.js
@@ -0,0 +1,32 @@
+var connect = require("connect"),
+  fs = require("fs"),
+  mustache = require("mustache");
+      
+connect(
+  connect.bodyParser(),
+  function(req, res) {
+    var userName = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+      },
+      // create and open the stream
+      tmplFile = fs.createReadStream(
+        __dirname + "/public/edit.html", 
+        {encoding: "utf8"}
+      ),
+      template = "",
+      html;
+      
+    tmplFile.on("data", function(data) {
+      template += data;
+    });
+    tmplFile.on("end", function() {
+      
+      // render the template with the userName object as data
+      html = mustache.to_html(template, userName);
+        
+      res.end(html);
+   
+    });
+  }
+).listen(8000);
